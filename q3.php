0f624423c8b2d1dd9aef69c800f36a4de27f88ad
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-11-21 17:38:14 +0530

    initial commit

---
+++ q3.php
@@ -0,0 +1,11 @@
+<?php
+$a = 10;
+$b = 5;
+echo"Addition=".($a+$b);
+echo "<br>";
+echo "substraction=".($a-$b);
+echo "<br>";
+echo "Multiplication=".($a*$b);
+echo "<br>";
+echo "Division=".($a/$b);
+?>
