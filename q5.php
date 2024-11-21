ab92485cda70554033521a02024671b5add4d4ca
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-11-21 17:39:02 +0530

    initial commit

---
+++ q5.php
@@ -0,0 +1,27 @@
+<?php
+$day = 3;
+switch ($day) {
+    case 1:
+        echo"monday";
+        break;
+    case 2:
+        echo "Tuesday";
+        break;
+
+    case 3:
+        echo "wednesday";
+        break;
+    case 4:
+        echo "thursday";
+        break;
+    case 5:
+        echo "friday";
+        break;
+    case 6:
+        echo "saturaday";
+        break;
+    case 7:
+        echo "sunday";
+        break;
+}
+?>
